edfe0fc63c78d4d4bd83b3be02edb70727cc4b60
Crowdbotics <engineering@crowdbotics.com>
2023-02-10 00:11:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/soft_leaf_dev_62486/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soft_leaf_dev_62486;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "soft_leaf_dev_62486";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-leaf-dev-62486",
+  url: "https://soft-leaf-dev-62486.botics.co",
+  api: "https://soft-leaf-dev-62486.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
